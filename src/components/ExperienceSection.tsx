1e2d7be5f637337ff8953981d0d3c102035b8127
Jason <me@jasonbarba.com>
2025-05-25 13:57:30 +0800

    maintain: update work exp and blog

--- src/components/ExperienceSection.tsx
+++ src/components/ExperienceSection.tsx
@@ -1,8 +1,8 @@
-import xsplit from '/public/xsplit.svg';
-import videocom from '/public/videocom.svg';
-import metrobank from '/public/metrobank.svg';
-import pangolin from '/public/pangolin.png';
-import asurion from '/public/asurion.png';
+import xsplit from "/public/xsplit.svg";
+import videocom from "/public/videocom.svg";
+import metrobank from "/public/metrobank.svg";
+import pangolin from "/public/pangolin.png";
+import asurion from "/public/asurion.png";
 import { StaticImageData } from "next/image";
 import * as WorkCard from "./workcard/WorkCard";
 
@@ -12,118 +12,179 @@ export interface WorkListProps {
   designation: string;
   highlights: string[];
   period: string;
-  technology: string[];
+  technology?: string[];
 }
 
 export function ContractSection() {
   const workList: WorkListProps[] = [
     {
       imageSource: asurion,
-      name: ['Asurion', 'https://asurion.com'],
-      designation: 'Software Engineer 3',
+      name: ["Asurion", "https://asurion.com"],
+      designation: "Software Engineer 3",
       highlights: [
         "Maintain and update public facing websites of Asurion domain",
         "Support on incidents covering public facing websites breakages",
-        "Feature ownership",
         "Facilitate communications with teams and stakeholders",
         "Code reviews",
+        "Feature ownership",
+        "Security SME",
       ],
-      period: '2023 - present',
-      technology: ["TypeScript", "Nx", "Gatsbyjs", "GraphQL", "Contentful", "Tailwind CSS", "NGINX", "Docker", "AWS", "Git", "Github Actions"],
+      period: "2023 - present",
+      // technology: [
+      //   "TypeScript",
+      //   "Nx",
+      //   "React",
+      //   "GraphQL",
+      //   "Contentful",
+      //   "Tailwind CSS",
+      //   "NGINX",
+      //   "Docker",
+      //   "AWS",
+      //   "Git",
+      //   "Github Actions",
+      // ],
     },
     {
       imageSource: videocom,
-      name: ['VideoCom', 'https://videocom.com'],
-      designation: 'Senior Software Engineer',
-      period: '2022 - 2023',
+      name: ["VideoCom", "https://videocom.com"],
+      designation: "Senior Software Engineer",
+      period: "2022 - 2023",
       highlights: [
         "Frontend core team that builds and maintains VideoCom Cloud",
         "Collaborated across departments to align implementation details for web and app teams.",
         "Worked on requirements deliberation with project managers and designers",
         "Created and maintained several features",
-        "Code reviews"
+        "Code reviews",
       ],
-      technology: ["Nextjs", "TypeScript", "GraphQL", "REST", "Tailwind CSS", "MySQL", "Redis", "Docker", "Kubernetes", "Cloudflare", "Google Cloud", "Git"],
+      technology: [
+        "Nextjs",
+        "TypeScript",
+        "GraphQL",
+        "REST",
+        "Tailwind CSS",
+        "MySQL",
+        "Redis",
+        "Docker",
+        "Kubernetes",
+        "Cloudflare",
+        "Google Cloud",
+        "Git",
+      ],
     },
     {
       imageSource: xsplit,
-      name: ['SplitmediaLabs', 'https://xsplit.com'],
-      designation: 'Software Engineer',
-      period: '2021 - 2022',
+      name: ["SplitmediaLabs", "https://xsplit.com"],
+      designation: "Software Engineer",
+      period: "2021 - 2022",
       highlights: [
         "Filled development deficiencies of XSplit Cloud",
         "Launched and managed various cloud features, focusing on UIs and API integrations.",
         "Worked with design teams for technical alignments",
       ],
-      technology: ["Nextjs", "TypeScript", "GraphQL", "Tailwind CSS", "MySQL", "Redis", "Docker", "Kubernetes", "Cloudflare", "Google Cloud", "Git"],
+      technology: [
+        "Nextjs",
+        "TypeScript",
+        "GraphQL",
+        "Tailwind CSS",
+        "MySQL",
+        "Redis",
+        "Docker",
+        "Kubernetes",
+        "Cloudflare",
+        "Google Cloud",
+        "Git",
+      ],
     },
     {
       imageSource: metrobank,
-      name: ['Metrobank', 'https://metrobank.com.ph'],
-      designation: 'Programmer Analyst',
-      period: '2020 - 2021',
+      name: ["Metrobank", "https://metrobank.com.ph"],
+      designation: "Programmer Analyst",
+      period: "2020 - 2021",
       highlights: [
         "Spearheaded frontend developement for buying and selling bonds",
         "Refactored, updated legacy dependencies",
         "Helped peers to minimize their development deficiencies",
       ],
-      technology: ['LoopBack 4', 'React', 'Material-UI', "SOAP", "REST", "Git"]
+      technology: ["LoopBack 4", "React", "Material-UI", "SOAP", "REST", "Git"],
     },
     {
       imageSource: pangolin,
-      name: ['TeamRed Security', 'https://pangolinsecured.com'],
-      designation: 'Software Developer',
-      period: '2019 - 2020',
+      name: ["TeamRed Security", "https://pangolinsecured.com"],
+      designation: "Software Developer",
+      period: "2019 - 2020",
       highlights: [
         "Worked with the founders on mobile designs and technical decisions",
         "UI migrations from react-native to webview",
         "Updated mobile codebase and its dependencies",
       ],
-      technology: ['React Native', 'TypeScript', 'WebView', "Python", "REST", "Jenkins", "Kubernetes", "Git"]
+      technology: [
+        "React Native",
+        "TypeScript",
+        "WebView",
+        "Python",
+        "REST",
+        "Jenkins",
+        "Kubernetes",
+        "Git",
+      ],
     },
   ];
 
   const legacyWork = {
-    name: ['Various tech companies & endeavours'],
-    designation: 'Developer',
-    period: '2017 - 2019',
+    name: ["Various tech companies & endeavours"],
+    designation: "Developer",
+    period: "2017 - 2019",
     highlights: [
-      'Created websites, landing pages and web scrapers as a freelancer',
-      'Worked with a fintech startup',
-      'Worked for a tech agency',
-      'Worked at one of the top retail banks'
+      "Created websites, landing pages and web scrapers as a freelancer",
+      "Worked with a fintech startup",
+      "Worked for a tech agency",
+      "Worked at one of the top retail banks",
     ],
-    technology: ['PHP', 'JavaScipt', 'TypeScript', 'React', 'Node', 'IBM API Connect', 'Firebase', "Git"]
-  }
+    technology: [
+      "PHP",
+      "JavaScipt",
+      "TypeScript",
+      "React",
+      "Node",
+      "IBM API Connect",
+      "Firebase",
+      "Git",
+    ],
+  };
 
   return (
     <section className="space-y-4 mx-auto" aria-describedby="exp">
       <h1 className={` font-semibold text-2xl sm:text-3xl`}>Work</h1>
-      <span className="text-xs sm:text-sm text-dark dark:text-white mb-4" id="exp">List of commercial work experiences</span>
-        <ul className="grid grid-cols-1 gap-y-4 mt-2">
+      <span
+        className="text-xs sm:text-sm text-dark dark:text-white mb-4"
+        id="exp"
+      >
+        List of commercial work experiences
+      </span>
+      <ul className="grid grid-cols-1 gap-y-4 mt-2">
         {workList.map((item, index) => (
           <li key={index} aria-label="list of companies">
             <WorkCard.Root
               className="w-full p-4 rounded-sm shadow border border-cgray border-opacity-20 relative text-dark dark:text-white"
               work={item}
             >
-              <WorkCard.Company className="flex"/>
-              <WorkCard.Responsibilities className="flex flex-col text-xs sm:text-sm mt-2"/>
-              <WorkCard.Technology className="text-xs sm:text-sm px-4 -ml-4 mt-4" />
-            </WorkCard.Root>
-          </li>
-        ))}
-          <li>
-            <WorkCard.Root
-              className="w-full p-4 rounded-sm shadow border border-cgray border-opacity-20 relative text-dark dark:text-white"
-              work={legacyWork}
-            >
-              <WorkCard.Company className="flex"/>
+              <WorkCard.Company className="flex" />
               <WorkCard.Responsibilities className="flex flex-col text-xs sm:text-sm mt-2" />
               <WorkCard.Technology className="text-xs sm:text-sm px-4 -ml-4 mt-4" />
             </WorkCard.Root>
           </li>
-        </ul>
+        ))}
+        <li>
+          <WorkCard.Root
+            className="w-full p-4 rounded-sm shadow border border-cgray border-opacity-20 relative text-dark dark:text-white"
+            work={legacyWork}
+          >
+            <WorkCard.Company className="flex" />
+            <WorkCard.Responsibilities className="flex flex-col text-xs sm:text-sm mt-2" />
+            <WorkCard.Technology className="text-xs sm:text-sm px-4 -ml-4 mt-4" />
+          </WorkCard.Root>
+        </li>
+      </ul>
     </section>
   );
 }
--- src/components/workcard/WorkCard.tsx
+++ src/components/workcard/WorkCard.tsx
@@ -1,15 +1,12 @@
-import Image from 'next/image';
-import {
-  BackpackIcon,
-  OpenInNewWindowIcon
-} from '@radix-ui/react-icons'
-import { forwardRef } from 'react';
-import { WorkCardContext, useWorkList } from './WorkCardContext';
-import { WorkListProps } from '../ExperienceSection';
+import Image from "next/image";
+import { BackpackIcon, OpenInNewWindowIcon } from "@radix-ui/react-icons";
+import { forwardRef } from "react";
+import { WorkCardContext, useWorkList } from "./WorkCardContext";
+import { WorkListProps } from "../ExperienceSection";
 
 const Root = forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { work: WorkListProps }  
+  React.HTMLAttributes<HTMLDivElement> & { work: WorkListProps }
 >(({ children, work, ...props }, ref) => {
   return (
     <WorkCardContext.Provider value={work}>
@@ -20,16 +17,19 @@ const Root = forwardRef<
   );
 });
 
-Root.displayName = 'WorkCardRoot';
+Root.displayName = "WorkCardRoot";
 
 const WorkCard = Root;
 
 const Company = forwardRef<
-HTMLDivElement,
-React.HTMLAttributes<HTMLDivElement>
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
 >(({ ...props }, ref) => {
   const workListContext = useWorkList();
-  let assistiveLabel = workListContext?.name[0] === 'Asurion' ? `Jason's current contract` : `one of Jason's past contract`;
+  let assistiveLabel =
+    workListContext?.name[0] === "Asurion"
+      ? `Jason's current contract`
+      : `one of Jason's past contract`;
   return (
     <div ref={ref} {...props}>
       <div className="w-12 h-12 relative">
@@ -39,12 +39,12 @@ React.HTMLAttributes<HTMLDivElement>
             quality={95}
             fill
             sizes="100vw"
-            className="object-cover object-left rounded" 
+            className="object-cover object-left rounded"
             alt="work_image"
             style={{ filter: "grayscale(90%)" }}
           />
         ) : (
-          <BackpackIcon className="w-12 h-12 text-gray-600 dark:text-gray-300"/>
+          <BackpackIcon className="w-12 h-12 text-gray-600 dark:text-gray-300" />
         )}
       </div>
       <div className="flex flex-col ml-2 text-xs sm:text-sm">
@@ -56,12 +56,16 @@ React.HTMLAttributes<HTMLDivElement>
             className="w-fit hover:underline hover:underline-offset-4"
             aria-label={`A link to ${workListContext?.name[0]}'s website, ${assistiveLabel}`}
           >
-            <span className="font-semibold mt-1">{workListContext?.name[0]}</span>
+            <span className="font-semibold mt-1">
+              {workListContext?.name[0]}
+            </span>
             <OpenInNewWindowIcon className="inline ml-1" />
           </a>
         ) : (
           <>
-            <span className="font-semibold mt-1">{workListContext?.name[0]}</span>
+            <span className="font-semibold mt-1">
+              {workListContext?.name[0]}
+            </span>
           </>
         )}
         <span className="dark:text-gray-300 ">{workListContext?.period}</span>
@@ -70,8 +74,7 @@ React.HTMLAttributes<HTMLDivElement>
   );
 });
 
-Company.displayName = 'WorkCardCompany';
-
+Company.displayName = "WorkCardCompany";
 
 const Responsibilities = forwardRef<
   HTMLDivElement,
@@ -81,16 +84,20 @@ const Responsibilities = forwardRef<
   return (
     <div ref={ref} {...props}>
       <span className="font-semibold mb-2">{workListContext?.designation}</span>
-      {workListContext?.highlights && workListContext?.highlights.map((exp, index) => (
-        <ul key={index} className="list-disc list-inside marker:text-[#d1d5db] dark:marker:text-[#4b5563]">
-          <li>{exp}</li>
-        </ul>
-      ))}
+      {workListContext?.highlights &&
+        workListContext?.highlights.map((exp, index) => (
+          <ul
+            key={index}
+            className="list-disc list-inside marker:text-[#d1d5db] dark:marker:text-[#4b5563]"
+          >
+            <li>{exp}</li>
+          </ul>
+        ))}
     </div>
   );
-})
+});
 
-Responsibilities.displayName = 'WorkCardResponsibilities';
+Responsibilities.displayName = "WorkCardResponsibilities";
 
 const Technology = forwardRef<
   HTMLDivElement,
@@ -99,11 +106,12 @@ const Technology = forwardRef<
   const workListContext = useWorkList();
   return (
     <div ref={ref} {...props}>
-      Technology: {workListContext?.technology.join(", ")}
+      {!!workListContext?.technology ? "Technology: " : null}{" "}
+      {workListContext?.technology?.join(", ")}
     </div>
-  )
+  );
 });
 
-Technology.displayName = 'WorkCardTechnology';
+Technology.displayName = "WorkCardTechnology";
 
 export { WorkCard, Root, Company, Responsibilities, Technology };
