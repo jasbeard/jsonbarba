7c0a009308536e5af81c5ace12c3535ba610a0e5
Jason <me@jasonbarba.com>
2025-08-04 00:25:17 +0800

    fix(blog tags): update tag responsive view

--- src/components/writing/Tags.tsx
+++ src/components/writing/Tags.tsx
@@ -1,11 +1,11 @@
 export const Tags = ({ list }: { list: string[] }) => {
   if (!list.length) return null;
   return (
-    <div className="flex space-x-2 pb-4 mb-2.5 text-xs md:text-sm">
+    <div className="flex space-x-2 pb-4 mb-2.5 text-xs md:text-sm gap-2 flex-wrap">
       {list.map((tag) => (
         <a
           key={tag}
-          className="py-1 px-4 font-medium border rounded-xl border-gray-500 text-gray-200 transition-all duration-500 hover:bg-indigo-500 cursor-pointer"
+          className="whitespace-nowrap !mx-0 py-1 px-4 font-medium border rounded-xl border-gray-500 text-gray-200 transition-all duration-500 hover:bg-indigo-500 cursor-pointer"
           href={`/writing/topic/${tag.toLowerCase()}`}
         >
           {tag}
@@ -13,4 +13,4 @@ export const Tags = ({ list }: { list: string[] }) => {
       ))}
     </div>
   );
-}
+};
