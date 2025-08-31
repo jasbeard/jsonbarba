9ab5e56db1fa9e4f7003ae1e94d3d5d4cfcbf9c8
Jason <me@jasonbarba.com>
2025-08-31 16:31:40 +0800

    feat: update header, and fix recent blog tag for search

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,16 +7,14 @@ export function Header() {
   return (
     <header className="w-full fixed top-0 bg-dark z-10">
       <div
-        className="w-full h-[100px] pointer-events-none select-none fixed top-0"
+        className="w-full h-[160px] pointer-events-none select-none fixed top-0"
         style={{
           backgroundImage: "linear-gradient(180deg, #000 20%, transparent)",
         }}
       ></div>
-      <div
-        className={`fixed flex top-0 left-0 right-0 justify-between ml-4 mt-2 ${serif.className}`}
-      >
-        <div className="w-full font-normal tracking-wide text-sm gap-2 -mt-2 pr-4 rounded-br bg-white dark:bg-dark">
-          <div className="flex items-center gap-4 mt-4 shadow-2xl">
+      <div className={`fixed w-full mt-2 ${serif.className}`}>
+        <div className="w-full flex justify-center font-normal tracking-wide text-sm gap-2 rounded-br bg-white dark:bg-dark">
+          <div className="flex items-center gap-8 py-4 shadow-2xl">
             <Link
               href="/"
               aria-label="home link of the website"
