7045323d4757e17f8d6272885b7ba881d80f353c
Jason <me@jasonbarba.com>
2025-11-02 17:22:29 +0800

    shadcn made me do it

---
+++ src/components/pictorial/getPhotos.ts
@@ -0,0 +1,51 @@
+// Landscape imports
+import nearGym from "/public/photographs/landscapes/an_area_where_I_go_to_the_gym.jpg";
+import nearCiti from "/public/photographs/landscapes/near_citi.jpg";
+import nearMytown from "/public/photographs/landscapes/near_mytown.jpg";
+import oreCentral from "/public/photographs/landscapes/ore_central.jpg";
+import walkway from "/public/photographs/landscapes/walkway.jpg";
+
+// Portrait imports
+import pocketPark from "/public/photographs/portraits/pocket_park.jpg";
+import catwalk from "/public/photographs/portraits/catwalk.jpg";
+import glassy from "/public/photographs/portraits/glassy.jpg";
+import cornerCat from "/public/photographs/portraits/corner_cat.jpg";
+import uptownMallBridge from "/public/photographs/portraits/traffic.jpg";
+import highStreetCat from "/public/photographs/portraits/high_street_cat_2.jpg";
+import pedxing from "/public/photographs/portraits/pedxing.jpg";
+import alley from "/public/photographs/portraits/some_alley.jpg";
+import tower from "/public/photographs/portraits/tower.jpg";
+import bgcParkFam from "/public/photographs/portraits/bgc_park_fam.jpg";
+import kidPet from "/public/photographs/portraits/kid_pet.jpg";
+import maInPark from "/public/photographs/portraits/ma_in_park2.jpg";
+import tree from "/public/photographs/portraits/tree_2.jpg";
+import wife from "/public/photographs/portraits/wife.jpg";
+import mc from "/public/photographs/portraits/mc.jpg";
+import cyberTruck from "/public/photographs/portraits/cybertruck.jpg";
+
+export {
+  // Landscape exports
+  nearGym,
+  nearCiti,
+  nearMytown,
+  oreCentral,
+  walkway,
+
+  // Portrait exports
+  pocketPark,
+  catwalk,
+  glassy,
+  cornerCat,
+  uptownMallBridge,
+  highStreetCat,
+  pedxing,
+  alley,
+  tower,
+  bgcParkFam,
+  kidPet,
+  maInPark,
+  tree,
+  wife,
+  mc,
+  cyberTruck,
+};
