3461cdb35ff397ff0e63c4bfcabf00640f4aaed1
Jason <me@jasonbarba.com>
2026-08-23 19:32:05 +0800

    fix(ts): fix config for next

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   const router = useRouter();
   return (
     <header className="w-full fixed top-0 z-10">
-      <div className="w-full h-16 md:h-20 pointer-events-none select-none fixed top-0 bg-gradient-to-b from-gofuniro from-30% to-transparent" />
+      <div className="w-full h-16 md:h-20 pointer-events-none select-none fixed top-0 bg-linear-to-b from-gofuniro from-30% to-transparent" />
       <div className={`fixed w-full mt-2 ${serif.className}`}>
         <div className="w-full flex justify-end text-lg font-[550] gap-2 px-4">
           <div className="flex items-center gap-4 md:gap-8 py-4 text-gray-950">
